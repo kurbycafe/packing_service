898b6f6bb466d745872cdc2cdad462a846a8ba83
kim <po04304a@gmail.com>
2025-12-22 02:13:53 +0100

    상품 가져오기 성공 유통기한 가격 표시 필요

--- src/main/java/com/dawayo/packing/Repository/ProductRepository.java
+++ src/main/java/com/dawayo/packing/Repository/ProductRepository.java
@@ -1,12 +1,22 @@
 package com.dawayo.packing.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.dawayo.packing.VO.ProductVO;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductVO, Long> {
     Optional<ProductVO> findByWooId(Long wooId);
+
+    @Query(value = "SELECT p.name, p.sku FROM product_table p WHERE p.name LIKE CONCAT('%', :query, '%') LIMIT 10", nativeQuery = true)
+    List<Object[]> searchProductsNative(@Param("query") String query);
+
+
+
 }
